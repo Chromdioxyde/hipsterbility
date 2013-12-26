c4cc499a28edf0139fc9212b0166a523fc79ed53
olivererxleben <oliver.erxleben@me.com>
2013-12-26 10:49:00 +0100

    code doc + structure added/updated

--- lib/Hipsterbility/src/de/hsosnabrueck/iui/informatik/olivererxleben/Hipsterbility.java
+++ lib/Hipsterbility/src/de/hsosnabrueck/iui/informatik/olivererxleben/Hipsterbility.java
@@ -2,29 +2,66 @@ package de.hsosnabrueck.iui.informatik.olivererxleben;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-
-import static android.app.PendingIntent.getActivity;
+import org.json.JSONObject;
 
 /**
- * Created by olivererxleben on 25.12.13.
+ * The Hipsterbility class is a monolithic wrapper for the Hipsterbility-library and implements all public methods which
+ * a user can use in own application.
  */
 public class Hipsterbility {
 
+    //================================================================================
+    // Properties
+    //================================================================================
+
+    JSONObject conf;
+
+    //================================================================================
+    // Constructors
+    //================================================================================
+
+    /**
+     * The default constructor to create a Hipsterbility object.
+     * It uses default values.
+     */
+    public Hipsterbility() {
+        // TODO: implementation
+    }
+
+    /**
+     * Parameterized constructor to create a Hipsterbility object.
+     * it evaluates a json object for configuration. For example
+     * @param conf : JSONObject
+     */
+    public Hipsterbility(JSONObject conf) {
+        // TODO: implementation
+    }
+
+    //================================================================================
+    // public Methods
+    //================================================================================
+
+
     public String test() {
+       // TODO: delete method, just for dev testing purposes
        return "hello Hipsterbility";
     }
 
     public void testAlert(Activity activity) {
+        // TODO: delete method, just for dev testing purposes
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 
-// 2. Chain together various setter methods to set the dialog characteristics
         builder.setMessage("Alert from lib")
                 .setTitle(this.test());
 
-// 3. Get the AlertDialog from create()
         AlertDialog dialog = builder.create();
 
         dialog.show();
     }
+
+    //================================================================================
+    // Private Methods
+    //================================================================================
+
 
 }
