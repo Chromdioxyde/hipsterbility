59c9bb403e624ba91ade4ce1e74796bde7532f8d
olivererxleben <oliver.erxleben@me.com>
2014-02-02 10:18:42 +0100

    added variable imlementation to converter class (now should implement converting function) added usage documentation to README in serverside

---
+++ serverside/README.md
@@ -0,0 +1,13 @@
+# Serverside - NodeJS Express Application
+
+## Usage
+
+* install node + npm
+* open terminal here
+* type 
+
+		npm install
+
+* after installation of all required packages: 
+
+		node app.js
--- serverside/classes/converter.js
+++ serverside/classes/converter.js
@@ -1,7 +1,60 @@
 var fluent_ffmpeg = require('fluent-ffmpeg');
 
-function Converter () {
-	// TODO implementation
+/*
+ * "constructor" function implements all need variable initialization 
+ */
+function Converter (camera_file, screen_captures, log, output ) {
+	// camera file/output shouldnt be undefined, should be an not empty string
+	this.camera_file = (typeof camera_file === 'string' && camera_file != '') ? camera_file : null;
+	this.output = (typeof output === 'string' && output != '') ? output : null;
+	
+	// screen captures should be an array > 0
+	this.screen_captures = (typeof screen_captures === 'array') ? screen_captures : null; 
+	
+	// logs should be an json (object)
+	this.logs = (typeof log === 'object') ? log : null;
+}
+
+/*
+ * Getters and Setters
+ * TODO add type check
+ */
+Converter.prototype = {
+	// Camera File Getters Setters
+	get cameraFile() {
+		return this._camera_file;
+	},
+	
+	set cameraFile(path) {
+		this._camera_file = path;
+	},
+	
+	// screen captures 
+	get screenCaptures() {
+		return this._screen_captures;
+	},
+	
+	set screenCaptures(screens) {
+		this._screencaptures = screens;
+	},
+	
+	// logs
+	get logs() {
+		return this._logs;
+	},
+	
+	set logs(logs) {
+		this._logs = logs;
+	},
+	
+	// output
+	get output() {
+		return this._camera_file;
+	},
+	
+	set output(path) {
+		this._camera_file = path;
+	}
 }
 
 /*
