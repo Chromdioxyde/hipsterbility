77d5ec6997cbcd0d78afe8439421bf4dc086b8d4
olivererxleben <oliver.erxleben@me.com>
2014-02-10 10:07:44 +0100

    logs method get all sessions added

--- serverside/routes/api/logs.js
+++ serverside/routes/api/logs.js
@@ -1,5 +1,15 @@
+var Query = require('../../classes/query');
+
 exports.all = function(req, res) {
-	// TODO implementation
+	var query = new Query;
+	
+	qstr = 'SELECT logs.idlogs, logs.file FROM logs WHERE sessions_idsessions = ' + req.params.session_id; 
+	console.log(qstr);
+	
+	query.execute(qstr, '', function(rows){
+		res.send(rows);
+	}); 
+	
 };
 
 exports.get = function(req, res) {
