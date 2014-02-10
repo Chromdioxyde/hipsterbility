ddcfeea71ffd75b9c6b3328cee00795d50854f57
olivererxleben <oliver.erxleben@me.com>
2014-02-10 10:08:04 +0100

    db init script updated

--- database/init.sql
+++ database/init.sql
@@ -6,26 +6,6 @@ CREATE SCHEMA IF NOT EXISTS `hipsterbility` DEFAULT CHARACTER SET latin1 COLLATE
 USE `hipsterbility` ;
 
 -- -----------------------------------------------------
--- Table `hipsterbility`.`captures`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`captures` (
-  `idcaptures` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  PRIMARY KEY (`idcaptures`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `hipsterbility`.`logs`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`logs` (
-  `idlogs` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  PRIMARY KEY (`idlogs`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `hipsterbility`.`users`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `hipsterbility`.`users` (
@@ -38,12 +18,72 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `hipsterbility`.`sessions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hipsterbility`.`sessions` (
+  `idsessions` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NULL ,
+  `active` TINYINT NULL DEFAULT 0 ,
+  `users_idusers` INT NULL ,
+  PRIMARY KEY (`idsessions`) ,
+  UNIQUE INDEX `idsessions_UNIQUE` (`idsessions` ASC) ,
+  INDEX `fk_sessions_users1_idx` (`users_idusers` ASC) ,
+  CONSTRAINT `fk_sessions_users1`
+    FOREIGN KEY (`users_idusers` )
+    REFERENCES `hipsterbility`.`users` (`idusers` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hipsterbility`.`captures`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hipsterbility`.`captures` (
+  `idcaptures` INT NOT NULL AUTO_INCREMENT ,
+  `file` TEXT NULL ,
+  `sessions_idsessions` INT NOT NULL ,
+  PRIMARY KEY (`idcaptures`) ,
+  INDEX `fk_captures_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_captures_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `hipsterbility`.`audios`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `hipsterbility`.`audios` (
   `idaudios` INT NOT NULL AUTO_INCREMENT ,
   `file` TEXT NULL ,
-  PRIMARY KEY (`idaudios`) )
+  `sessions_idsessions` INT NOT NULL ,
+  PRIMARY KEY (`idaudios`) ,
+  INDEX `fk_audios_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_audios_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `hipsterbility`.`logs`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `hipsterbility`.`logs` (
+  `idlogs` INT NOT NULL AUTO_INCREMENT ,
+  `file` TEXT NULL ,
+  `sessions_idsessions` INT NOT NULL ,
+  PRIMARY KEY (`idlogs`) ,
+  INDEX `fk_logs_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_logs_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -53,7 +93,14 @@ ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `hipsterbility`.`videos` (
   `idvideos` INT NOT NULL AUTO_INCREMENT ,
   `file` TEXT NULL ,
-  PRIMARY KEY (`idvideos`) )
+  `sessions_idsessions` INT NOT NULL ,
+  PRIMARY KEY (`idvideos`) ,
+  INDEX `fk_videos_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_videos_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -64,18 +111,14 @@ CREATE  TABLE IF NOT EXISTS `hipsterbility`.`results` (
   `idresults` INT NOT NULL ,
   `file` TEXT NULL ,
   `timestamp` TIMESTAMP NULL ,
-  PRIMARY KEY (`idresults`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `hipsterbility`.`tasks`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`tasks` (
-  `idtasks` INT NOT NULL AUTO_INCREMENT ,
-  `name` TEXT NULL ,
-  `done` TINYINT NULL DEFAULT 0 ,
-  PRIMARY KEY (`idtasks`) )
+  `sessions_idsessions` INT NOT NULL ,
+  PRIMARY KEY (`idresults`) ,
+  INDEX `fk_results_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_results_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -87,71 +130,28 @@ CREATE  TABLE IF NOT EXISTS `hipsterbility`.`todos` (
   `name` TEXT NULL ,
   `description` TEXT NULL ,
   `active` TINYINT NULL ,
-  `tasks_idtasks` INT NOT NULL ,
+  `sessions_idsessions` INT NOT NULL ,
   PRIMARY KEY (`idtodos`) ,
-  INDEX `fk_todos_tasks1_idx` (`tasks_idtasks` ASC) ,
-  CONSTRAINT `fk_todos_tasks1`
-    FOREIGN KEY (`tasks_idtasks` )
-    REFERENCES `hipsterbility`.`tasks` (`idtasks` )
+  INDEX `fk_todos_sessions1_idx` (`sessions_idsessions` ASC) ,
+  CONSTRAINT `fk_todos_sessions1`
+    FOREIGN KEY (`sessions_idsessions` )
+    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `hipsterbility`.`sessions`
+-- Table `hipsterbility`.`tasks`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`sessions` (
-  `idsessions` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
-  `active` TINYINT NULL DEFAULT 0 ,
-  `captures_idcaptures` INT NOT NULL ,
-  `logs_idlogs` INT NOT NULL ,
-  `users_idusers` INT NOT NULL ,
-  `audios_idaudios1` INT NOT NULL ,
-  `videos_idvideos` INT NOT NULL ,
-  `results_idresults` INT NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `hipsterbility`.`tasks` (
+  `idtasks` INT NOT NULL AUTO_INCREMENT ,
+  `name` TEXT NULL ,
+  `done` TINYINT NULL DEFAULT 0 ,
   `todos_idtodos` INT NOT NULL ,
-  PRIMARY KEY (`idsessions`) ,
-  UNIQUE INDEX `idsessions_UNIQUE` (`idsessions` ASC) ,
-  INDEX `fk_sessions_captures1_idx` (`captures_idcaptures` ASC) ,
-  INDEX `fk_sessions_logs1_idx` (`logs_idlogs` ASC) ,
-  INDEX `fk_sessions_users1_idx` (`users_idusers` ASC) ,
-  INDEX `fk_sessions_audios1_idx` (`audios_idaudios1` ASC) ,
-  INDEX `fk_sessions_videos1_idx` (`videos_idvideos` ASC) ,
-  INDEX `fk_sessions_results1_idx` (`results_idresults` ASC) ,
-  INDEX `fk_sessions_todos1_idx` (`todos_idtodos` ASC) ,
-  CONSTRAINT `fk_sessions_captures1`
-    FOREIGN KEY (`captures_idcaptures` )
-    REFERENCES `hipsterbility`.`captures` (`idcaptures` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_logs1`
-    FOREIGN KEY (`logs_idlogs` )
-    REFERENCES `hipsterbility`.`logs` (`idlogs` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_users1`
-    FOREIGN KEY (`users_idusers` )
-    REFERENCES `hipsterbility`.`users` (`idusers` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_audios1`
-    FOREIGN KEY (`audios_idaudios1` )
-    REFERENCES `hipsterbility`.`audios` (`idaudios` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_videos1`
-    FOREIGN KEY (`videos_idvideos` )
-    REFERENCES `hipsterbility`.`videos` (`idvideos` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_results1`
-    FOREIGN KEY (`results_idresults` )
-    REFERENCES `hipsterbility`.`results` (`idresults` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_sessions_todos1`
+  PRIMARY KEY (`idtasks`) ,
+  INDEX `fk_tasks_todos1_idx` (`todos_idtodos` ASC) ,
+  CONSTRAINT `fk_tasks_todos1`
     FOREIGN KEY (`todos_idtodos` )
     REFERENCES `hipsterbility`.`todos` (`idtodos` )
     ON DELETE NO ACTION
