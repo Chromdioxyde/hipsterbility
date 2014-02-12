d24e69f1cf7f15ba7dabd62b7a32f4c0dbe7c73f
Chromdioxyde <chromdioxyde@gmail.com>
2014-02-12 11:00:08 +0100

    Revert "get method for specific logfile for a session added"
    This reverts commit c28479fef5aaef2476c498044c70bb85c7c6897f.

--- serverside/routes/api/logs.js
+++ serverside/routes/api/logs.js
@@ -1,30 +1,21 @@
 var Query = require('../../classes/query');
 
-/**
- *
- */
 exports.all = function(req, res) {
 	var query = new Query;
+	
 	qstr = 'SELECT logs.idlogs, logs.file FROM logs WHERE sessions_idsessions = ' + req.params.session_id; 
+	console.log(qstr);
+	
 	query.execute(qstr, '', function(rows){
 		res.send(rows);
 	}); 
+	
 };
 
-/**
- *
- */
 exports.get = function(req, res) {
-	var query = new Query;
-	qstr = 'SELECT logs.idlogs, logs.file FROM logs WHERE sessions_idsessions = ' + req.params.session_id + ' AND idlogs =' + req.params.id_logs;
-	query.execute(qstr, '', function(rows) {
-		res.send(rows);
-	});
+	// TODO implementation
 };
 
-/**
- *
- */
 exports.post = function(req, res) {
 	// TODO implementation
 };
