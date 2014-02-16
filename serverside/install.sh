d32e2d3814b44dac791c474e718069db1d632a92
Oliver Erxleben <oliver.erxleben@me.com>
2014-02-16 10:37:06 +0100

    ffmpeg added to installs added TODO

--- serverside/install.sh
+++ serverside/install.sh
@@ -1,8 +1,10 @@
 # install.sh
 # always install latest stuff 
+
+
 sudo apt-get update 
 
-sudo apt-get install mysql-server
+sudo apt-get install mysql-server ffmpeg
 
 sudo apt-get install python g++ make checkinstall
 src=$(mktemp -d) && cd $src
@@ -11,3 +13,5 @@ tar xzvf node-latest.tar.gz && cd node-v*
 ./configure
 sudo checkinstall -y --install=no --pkgversion 0.10.25  make -j$(($(nproc)+1)) install  # Replace with current version number.
 sudo dpkg -i node_*
+
+# TODO run db init script
