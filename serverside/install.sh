f5a6266d58600b3e77cfa4cbb25e970f4e157cac
Oliver Erxleben <oliver.erxleben@me.com>
2014-02-16 10:34:03 +0100

    added vagrant config

---
+++ serverside/install.sh
@@ -0,0 +1,13 @@
+# install.sh
+# always install latest stuff 
+sudo apt-get update 
+
+sudo apt-get install mysql-server
+
+sudo apt-get install python g++ make checkinstall
+src=$(mktemp -d) && cd $src
+wget -N http://nodejs.org/dist/node-latest.tar.gz
+tar xzvf node-latest.tar.gz && cd node-v*
+./configure
+sudo checkinstall -y --install=no --pkgversion 0.10.25  make -j$(($(nproc)+1)) install  # Replace with current version number.
+sudo dpkg -i node_*
