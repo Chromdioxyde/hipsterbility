8cb702771b6bbadfeb4e914bbc4abd9db1b1938b
Oliver Erxleben <oliver.erxleben@me.com>
2014-02-17 22:43:37 +0100

    updated init.sql script with some test values

--- database/init.sql
+++ database/init.sql
@@ -2,164 +2,334 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `hipsterbility` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `hipsterbility` ;
+CREATE DATABASE  IF NOT EXISTS `hipsterbility` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `hipsterbility`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: localhost    Database: hipsterbility
+-- ------------------------------------------------------
+-- Server version 5.5.35-0+wheezy1
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`users`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`users` (
-  `idusers` INT NOT NULL AUTO_INCREMENT ,
-  `name` TEXT NULL ,
-  `password` TEXT NULL ,
-  `active` TINYINT NULL DEFAULT 0 ,
-  PRIMARY KEY (`idusers`) )
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `apps`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`sessions`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`sessions` (
-  `idsessions` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
-  `active` TINYINT NULL DEFAULT 0 ,
-  `users_idusers` INT NULL ,
-  PRIMARY KEY (`idsessions`) ,
-  UNIQUE INDEX `idsessions_UNIQUE` (`idsessions` ASC) ,
-  INDEX `fk_sessions_users1_idx` (`users_idusers` ASC) ,
-  CONSTRAINT `fk_sessions_users1`
-    FOREIGN KEY (`users_idusers` )
-    REFERENCES `hipsterbility`.`users` (`idusers` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `apps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `apps` (
+  `idapps` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idapps`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `apps`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`captures`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`captures` (
-  `idcaptures` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idcaptures`) ,
-  INDEX `fk_captures_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_captures_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `apps` WRITE;
+/*!40000 ALTER TABLE `apps` DISABLE KEYS */;
+INSERT INTO `apps` VALUES (1,'testApp'),(2,'Fancyness');
+/*!40000 ALTER TABLE `apps` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `audios`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`audios`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`audios` (
-  `idaudios` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idaudios`) ,
-  INDEX `fk_audios_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_audios_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `audios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `audios` (
+  `idaudios` int(11) NOT NULL AUTO_INCREMENT,
+  `file` text,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idaudios`),
+  KEY `fk_audios_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_audios_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `audios`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`logs`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`logs` (
-  `idlogs` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idlogs`) ,
-  INDEX `fk_logs_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_logs_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `audios` WRITE;
+/*!40000 ALTER TABLE `audios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `audios` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `captures`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`videos`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`videos` (
-  `idvideos` INT NOT NULL AUTO_INCREMENT ,
-  `file` TEXT NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idvideos`) ,
-  INDEX `fk_videos_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_videos_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `captures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `captures` (
+  `idcaptures` int(11) NOT NULL AUTO_INCREMENT,
+  `file` text,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idcaptures`),
+  KEY `fk_captures_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_captures_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `captures`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`results`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`results` (
-  `idresults` INT NOT NULL ,
-  `file` TEXT NULL ,
-  `timestamp` TIMESTAMP NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idresults`) ,
-  INDEX `fk_results_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_results_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `captures` WRITE;
+/*!40000 ALTER TABLE `captures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `captures` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `devices`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`todos`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`todos` (
-  `idtodos` INT NOT NULL AUTO_INCREMENT ,
-  `name` TEXT NULL ,
-  `description` TEXT NULL ,
-  `active` TINYINT NULL ,
-  `sessions_idsessions` INT NOT NULL ,
-  PRIMARY KEY (`idtodos`) ,
-  INDEX `fk_todos_sessions1_idx` (`sessions_idsessions` ASC) ,
-  CONSTRAINT `fk_todos_sessions1`
-    FOREIGN KEY (`sessions_idsessions` )
-    REFERENCES `hipsterbility`.`sessions` (`idsessions` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `devices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `devices` (
+  `iddevices` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `os_version` varchar(10) DEFAULT NULL,
+  `type` text,
+  `users_idusers` int(11) NOT NULL,
+  PRIMARY KEY (`iddevices`),
+  KEY `fk_devices_users1_idx` (`users_idusers`),
+  CONSTRAINT `fk_devices_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `devices`
+--
 
--- -----------------------------------------------------
--- Table `hipsterbility`.`tasks`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `hipsterbility`.`tasks` (
-  `idtasks` INT NOT NULL AUTO_INCREMENT ,
-  `name` TEXT NULL ,
-  `done` TINYINT NULL DEFAULT 0 ,
-  `todos_idtodos` INT NOT NULL ,
-  PRIMARY KEY (`idtasks`) ,
-  INDEX `fk_tasks_todos1_idx` (`todos_idtodos` ASC) ,
-  CONSTRAINT `fk_tasks_todos1`
-    FOREIGN KEY (`todos_idtodos` )
-    REFERENCES `hipsterbility`.`todos` (`idtodos` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+INSERT INTO `devices` VALUES (1,'Nexus 5','4.4.2','phone',1);
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `logs`
+--
 
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logs` (
+  `idlogs` int(11) NOT NULL AUTO_INCREMENT,
+  `file` text,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idlogs`),
+  KEY `fk_logs_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_logs_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `results`
+--
+
+DROP TABLE IF EXISTS `results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `results` (
+  `idresults` int(11) NOT NULL,
+  `file` text,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idresults`),
+  KEY `fk_results_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_results_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `results`
+--
+
+LOCK TABLES `results` WRITE;
+/*!40000 ALTER TABLE `results` DISABLE KEYS */;
+/*!40000 ALTER TABLE `results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `idsessions` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT '0',
+  `users_idusers` int(11) DEFAULT NULL,
+  `apps_idapps` int(11) NOT NULL,
+  `devices_iddevices` int(11) NOT NULL,
+  PRIMARY KEY (`idsessions`),
+  UNIQUE KEY `idsessions_UNIQUE` (`idsessions`),
+  KEY `fk_sessions_users1_idx` (`users_idusers`),
+  KEY `fk_sessions_apps1_idx` (`apps_idapps`),
+  KEY `fk_sessions_devices1_idx` (`devices_iddevices`),
+  CONSTRAINT `fk_sessions_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_sessions_apps1` FOREIGN KEY (`apps_idapps`) REFERENCES `apps` (`idapps`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_sessions_devices1` FOREIGN KEY (`devices_iddevices`) REFERENCES `devices` (`iddevices`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `sessions` VALUES (1,'Hello World',0,1,1,1),(2,'Another Test',0,1,2,1);
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `idtasks` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `done` tinyint(4) DEFAULT '0',
+  `todos_idtodos` int(11) NOT NULL,
+  PRIMARY KEY (`idtasks`),
+  KEY `fk_tasks_todos1_idx` (`todos_idtodos`),
+  CONSTRAINT `fk_tasks_todos1` FOREIGN KEY (`todos_idtodos`) REFERENCES `todos` (`idtodos`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'rtfm',0,1),(2,'do some stuff',0,1),(3,'rtfm again',0,1);
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `todos`
+--
+
+DROP TABLE IF EXISTS `todos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `todos` (
+  `idtodos` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `description` text,
+  `active` tinyint(4) DEFAULT NULL,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idtodos`),
+  KEY `fk_todos_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_todos_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todos`
+--
+
+LOCK TABLES `todos` WRITE;
+/*!40000 ALTER TABLE `todos` DISABLE KEYS */;
+INSERT INTO `todos` VALUES (1,'My List','A list of things to do',0,1);
+/*!40000 ALTER TABLE `todos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `password` text,
+  `active` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`idusers`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Tobi Tester','1234',1),(2,'Susi Sorglos','4321',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `videos` (
+  `idvideos` int(11) NOT NULL AUTO_INCREMENT,
+  `file` text,
+  `sessions_idsessions` int(11) NOT NULL,
+  PRIMARY KEY (`idvideos`),
+  KEY `fk_videos_sessions1_idx` (`sessions_idsessions`),
+  CONSTRAINT `fk_videos_sessions1` FOREIGN KEY (`sessions_idsessions`) REFERENCES `sessions` (`idsessions`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `videos`
+--
+
+LOCK TABLES `videos` WRITE;
+/*!40000 ALTER TABLE `videos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `videos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-02-17 22:41:03
