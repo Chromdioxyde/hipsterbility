34b80e9fed8c8e8e2b155bb83bb957e27c2a1a90
Oliver Erxleben <oliver.erxleben@me.com>
2014-02-23 16:43:43 +0100

    replaced ffmpeg wrapper with avconv, added images to video function

--- serverside/classes/converter.js
+++ serverside/classes/converter.js
@@ -1,11 +1,22 @@
-var fluent_ffmpeg = require('fluent-ffmpeg');
 
+var avconv = require('avconv');
 /*
  * "constructor" function implements all need variable initialization 
  */
-function Converter (camera_file, screen_captures, log, output ) {
-	// camera file/output shouldnt be undefined, should be an not empty string
-	this.camera_file = (typeof camera_file === 'string' && camera_file != '') ? camera_file : null;
+function Converter (camera_files, screen_captures, log, output ) {
+
+	// if camera_files is array just add it
+	if ( typeof camera_files === '[object Array]' ) {
+		this.camera_files = camera_files;
+	}
+
+	// if camera_files is a string, we create an array and add em to it.
+	if ( typeof camera_files === '[object String]') {
+		this.camera_files = [];
+		this.camera_files.push(camera_files);
+	}
+
+	// output shouldnt be undefined, should be an not empty string
 	this.output = (typeof output === 'string' && output != '') ? output : null;
 	
 	// screen captures should be an array > 0
@@ -13,7 +24,20 @@ function Converter (camera_file, screen_captures, log, output ) {
 	
 	// logs should be an json (object)
 	this.logs = (typeof log === 'object') ? log : null;
-}
+
+	this.proc = new ffmpeg({
+		// input source, required
+		source: this.camera_files[0],
+		// timout of the spawned ffmpeg sub-processes in seconds (optional, defaults to 30)
+		timeout: 300,
+		// default priority for all ffmpeg sub-processes (optional, defaults to 0 which is no priorization)
+		priority: 0,
+		// set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
+		logger: null,
+		// completely disable logging (optional, defaults to false)
+		nolog: false,
+
+	});}
 
 /*
  * Getters and Setters
@@ -21,11 +45,12 @@ function Converter (camera_file, screen_captures, log, output ) {
  */
 Converter.prototype = {
 	// Camera File Getters Setters
-	get cameraFile() {
+
+	get cameraFiles() {
 		return this._camera_file;
 	},
 	
-	set cameraFile(path) {
+	set cameraFiles(path) {
 		this._camera_file = path;
 	},
 	
@@ -57,11 +82,73 @@ Converter.prototype = {
 	}
 }
 
+Converter.prototype.addCameraFile = function(camera_file) {
+
+	if ( typeof camera_files === '[object String]') {
+		this.camera_files = [];
+		this.camera_files.push(camera_files);
+	}
+
+}
+
+Converter.prototype.addScreenshot = function(screenshot) {
+	this.screenCaptures.push(screenshot);
+}
+
+/**
+ *
+ * creates a video file from screenshots
+ *  
+ *
+ */
+Converter.prototype.createVideoFromScreenshots = function(user, session, intervall) {
+	var input = '/uploads/'+user+'/'+session+'/images/&d.jpg'; // 
+	var output = '/uploads/'+user+'/'+session+'/images/result.mpg';
+
+	// avconv parameters
+	var params = [
+		'-r', intervall,
+		'-i', input,
+		'-r', '25',
+		'-s', '720x1280',
+		'-vsync', 'cfr',
+		output
+	];
+
+	var stream = avconv(params);
+
+	stream.on('data', function(data) {
+	    // handling
+	});
+
+	stream.on('progress', function(progress) {
+	    /*
+	    Progress is a floating number between 0 ... 1 that keeps you
+	    informed about the current avconv conversion process.
+	    */
+	});
+
+	stream.once('end', function(exitCode, signal) {
+		console.log('fin ' + exitCode);
+		// errorhandling
+	});
+}
+
+/**
+ *
+ * Creates a video from captured videos 
+ *
+ */
+Converter.prototype.createVideoFromVideos = function() {
+
+}
+
 /*
  * converts video files into one resultsvideo 
  * 
  */
 Converter.prototype.createResults = function () {
 	// TODO implementation
-};
 
+
+};
