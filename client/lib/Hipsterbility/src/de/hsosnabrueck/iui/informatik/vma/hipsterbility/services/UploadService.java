95a40e2667bf561f0e687d1f875d876c0d6aaa0a
Albert Hoffmann <chromdioxyde@gmail.com>
2014-04-10 21:50:28 +0200

    Added comments to upload service and further documentation

--- client/lib/Hipsterbility/src/de/hsosnabrueck/iui/informatik/vma/hipsterbility/services/UploadService.java
+++ client/lib/Hipsterbility/src/de/hsosnabrueck/iui/informatik/vma/hipsterbility/services/UploadService.java
@@ -1,7 +1,6 @@
 package de.hsosnabrueck.iui.informatik.vma.hipsterbility.services;
 
 import android.app.*;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -17,40 +16,36 @@ import org.apache.http.Header;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by Albert Hoffmann on 25.02.14.
+ * Created on 25.02.14.
+ *
+ * Service to handle data uploads to the server after a session has finished.
+ *
  */
 public class UploadService extends Service {
 
+    // Constants
     private final static String TAG = UploadService.class.getName();
     private final static int NOTIFICATION_ID = 3;
-    //TODO: set parameter names
+    //TODO: set parameter names for future file types
     private final static String PARAM_NAME_SCREENSHOT = "screenshot";
     private final static String PARAM_NAME_CAMERA = "video";
     private final static String PARAM_NAME_SCREEN_RECORDING = "";
     private final static String PARAM_NAME_TOUCH_LOG = "";
     private final static String PARAM_NAME_AUDIO_CAPTURE = "";
+
     private volatile int totalBytes = 0;
     private volatile int bytesDone = 0;
-    private int fileTotalCount = 0;
+    private int totalFileCount = 0;
     private volatile int fileDoneCount = 0;
     private Session session;
     private NotificationManager notificationManager;
-    private AlertDialog alertDialog;
 
     private HashMap<String, List<File>> files;
-
-//    private ArrayList<File> screencaptureFileList = new ArrayList<File>();
-//    private ArrayList<File> audioFileList = new ArrayList<File>();
-//    private ArrayList<File> lifecycleFileList = new ArrayList<File>();
-//    private ArrayList<File> cameraFilesList = new ArrayList<File>();
-//    private ArrayList<File> screenshotFileList = new ArrayList<File>();
-//    private ArrayList<File> touchLogFileList = new ArrayList<File>();
 
     /**
      * private constructor for static singleton.
@@ -78,10 +73,14 @@ public class UploadService extends Service {
         }
     }
 
+    /**
+     * Lists files in the subdirectories of the base session folder and stores them in the HashMap files
+     * where the directory name serves as key.
+     */
     private void listFiles() {
         File sessionDir = new File(Util.createSessionDirPathName(session.getId()));
         totalBytes = 0;
-        fileTotalCount = 0;
+        totalFileCount = 0;
         if (!sessionDir.exists()) {
             Log.e(TAG, "Session directory does not exist: " + sessionDir.getAbsolutePath());
             return;
@@ -91,7 +90,7 @@ public class UploadService extends Service {
         for (File f : subdirs) {
             if (f.isDirectory()) {
                 List<File> fileList = Arrays.asList(f.listFiles());
-                fileTotalCount += fileList.size();
+                totalFileCount += fileList.size();
                 files.put(f.getName(), fileList);
                 for (File fs : fileList) {
                     if (fs != null) {
@@ -100,13 +99,17 @@ public class UploadService extends Service {
                 }
             }
         }
-        Log.d(TAG, "Overall size of files: " + totalBytes + " bytes");
+        Log.d(TAG, "Files: " + totalFileCount + "Bytes: " + totalBytes);
     }
 
     private void updateSession() {
         this.session = SessionManager.getInstace().getSessionInProgress();
     }
 
+    /**
+     * Creates a notification which is used as a trigger to upload files when tapped.
+     * Notification can be dismissed by sliding.
+     */
     private void createUploadNotification() {
         updateSession();
         String text = getString(R.string.notification_text_upload_data)
@@ -136,7 +139,7 @@ public class UploadService extends Service {
         }
     }
 
-    private boolean uploadFiles(Session session, List<File> mFilesList, String suffix, String paramName) {
+    private void uploadFiles(Session session, List<File> mFilesList, String suffix, String paramName) {
         for (File f : mFilesList) {
             RequestParams params = new RequestParams();
             try {
@@ -144,24 +147,20 @@ public class UploadService extends Service {
             } catch (FileNotFoundException e) {
                 Log.e(TAG, e.getMessage());
             }
-            postFile(Util.createRelativeRoute(session.getUser(), session, suffix), params);
+            FileUploadAsyncHttpResponseHandler responseHandler = new FileUploadAsyncHttpResponseHandler();
+            HipsterbilityRestClient.post(
+                    Util.createRelativeRoute(session.getUser(), session, suffix),
+                    params, responseHandler);
         }
-
-        return true;
     }
 
-    private boolean postFile(String relUrl, RequestParams params) {
-        FileUploadAsyncHttpResponseHandler responseHandler = new FileUploadAsyncHttpResponseHandler();
-        HipsterbilityRestClient.post(relUrl, params, responseHandler);
-        return responseHandler.isSuccess();
-    }
 
     private synchronized void updateProgress() {
         Notification.Builder builder = new Notification.Builder(this);
         builder.setSmallIcon(R.drawable.ic_stat_main);
-        builder.setProgress(fileTotalCount, fileDoneCount, false);
+        builder.setProgress(totalFileCount, fileDoneCount, false);
         builder.setContentTitle(getString(R.string.notification_title_upload_data));
-        builder.setContentText("Files complete: " +fileDoneCount + " / " + fileTotalCount);
+        builder.setContentText("Files complete: " +fileDoneCount + " / " + totalFileCount);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
@@ -181,6 +180,11 @@ public class UploadService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+    /**
+     * Sends a PUT request to the server on the current session resource to signal successful upload of all files.
+     * Creates a notification afterwards, which deletes the uploaded files of tap.
+     * TODO: Automatically delete uploaded files after required testing.
+     */
     private synchronized void finishUpload(){
         RequestParams params = new RequestParams();
         params.add("finished", "1");
@@ -204,25 +208,23 @@ public class UploadService extends Service {
 
     }
 
+    /**
+     * Custom response handler for file uploads.
+     * Used for progress update and to check if all files were successfully uploaded.
+     * TODO: Better error handing if connection to the server is cut off.
+     */
     private class FileUploadAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
         private final String TAG = FileUploadAsyncHttpResponseHandler.class.getName();
-        private boolean success = false;
-        public boolean isSuccess() {
-            return success;
-        }
 
         @Override
         public void onStart() {
             // Initiated the request
-            //TODO: do something or delete
             Log.d(TAG, "Request started");
         }
 
         @Override
         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
             // Successfully got a response
-            //TODO: do something or delete
-            success = true;
             fileDoneCount++;
             updateProgress();
             Log.d(TAG, "Request successful, status code: " + statusCode);
@@ -231,30 +233,26 @@ public class UploadService extends Service {
         @Override
         public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
             // Response failed :(
-            //TODO: do something else on failure
             Log.d(TAG, "Request failed, status code: " + statusCode + ", error: " + error.getMessage());
-            success = false;
         }
 
         @Override
         public void onRetry() {
             // Request was retried
-            //TODO: do something or delete
             Log.d(TAG, "Request retry");
         }
 
         @Override
         public void onProgress(int bytesWritten, int totalSize) {
             // Progress notification
-            //TODO: do something or delete, notify user
             bytesDone += bytesWritten;
         }
 
         @Override
         public void onFinish() {
             // Completed the request (either success or failure)
-            //TODO: do something or delete
-            if (fileTotalCount == fileDoneCount) {
+            // Check if all files were uploaded successfully
+            if (totalFileCount == fileDoneCount) {
                finishUpload();
             }
             Log.d(TAG, "Request finished");
